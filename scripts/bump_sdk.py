fb69e5daefeec525baacf3a016300d56b43b7ff3
d299techie <d299techie@gmail.com>
2026-08-13 17:24:05 +0530

    Use SDK 34 instead of 35 (broken on runner), install platform explicitly

--- scripts/bump_sdk.py
+++ scripts/bump_sdk.py
@@ -4,10 +4,10 @@ with open('android/variables.gradle') as f:
     c = f.read()
 
 c = re.sub(r'minSdkVersion\s*=\s*\d+', 'minSdkVersion = 24', c)
-c = re.sub(r'compileSdkVersion\s*=\s*\d+', 'compileSdkVersion = 35', c)
-c = re.sub(r'targetSdkVersion\s*=\s*\d+', 'targetSdkVersion = 35', c)
-c = re.sub(r'compileSdk\s*=\s*\d+', 'compileSdk = 35', c)
-c = re.sub(r'targetSdk\s*=\s*\d+', 'targetSdk = 35', c)
+c = re.sub(r'compileSdkVersion\s*=\s*\d+', 'compileSdkVersion = 34', c)
+c = re.sub(r'targetSdkVersion\s*=\s*\d+', 'targetSdkVersion = 34', c)
+c = re.sub(r'compileSdk\s*=\s*\d+', 'compileSdk = 34', c)
+c = re.sub(r'targetSdk\s*=\s*\d+', 'targetSdk = 34', c)
 
 with open('android/variables.gradle', 'w') as f:
     f.write(c)
